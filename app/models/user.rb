d6998db1f6c63089067ab4985ca26011fb5a3eae
kaleid-eminA <kaleidliner.illyasviel@gmail.com>
2021-06-05 13:51:34 +0900

    ユーザー認証機能　テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-           VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-         validates :password, format: { with: VALID_PASSWORD_REGEX }
-         validates :nickname, presence: true
-         validates :family_name, presence: true
-         validates :first_name, presence: true
+           VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+           VALID_JP_NAME_REGEX = /\A[一-龥ぁ-ん]/
            VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-         validates :family_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
-         validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+           
+         validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-         validates :birth_date, presence: true
-
+         with_options presence: true do
+           validates :nickname
+           validates :family_name, format: { with: VALID_JP_NAME_REGEX }
+           validates :first_name, format: { with: VALID_JP_NAME_REGEX }
+           validates :family_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+           validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+           validates :birth_date
+         end
 
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#create' do
+  describe '新規登録できるとき' do
     before do
       @user = FactoryBot.build(:user)
     end
-
+  context "新規登録できないとき" do
     it "nicknameが空だと登録できない" do
       @user.nickname = nil
       @user.valid?
@@ -45,7 +45,7 @@ RSpec.describe User, type: :model do
     it "passwordは半角英数字混合がないと登録できない（数字）" do
       @user.password = '123456'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password is invalid")
     end
     it "passwordとpasswrod(確認)は一致していることが必須である" do
       @user.password = '123abc'
@@ -89,4 +89,77 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Birth date can't be blank")
     end
   end
-end
+
+    context "新規登録できるとき"
+    it "すべての項目が存在すれば登録できる" do
+      expect(@user).to be_valid
+    end
+    it "ニックネームが空でなければ登録できる" do
+      @user.nickname = FactoryBot.build(:user, nickname: @user.nickname)
+      expect(@user).to be_valid
+    end
+    it "メールアドレスが空でなければ登録できる" do
+      @user.email = "email@com"
+      expect(@user).to be_valid
+    end
+    it "メールアドレスに一意性があれば登録できる" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      expect(@user).to be_valid
+    end
+    it "メールアドレスに@があれば登録できる" do
+      @user.email = "email@com"
+      expect(@user).to be_valid
+    end
+    it "パスワードが空でなければ登録できる" do
+      @user.password = "123abc"
+      @user.password_confirmation = '123abc'
+      expect(@user).to be_valid
+    end
+    it "パスワードが6文字以上であれば登録できる" do
+      @user.password = "abc123"
+      @user.password_confirmation = 'abc123'
+      expect(@user).to be_valid
+    end
+    it "パスワードは半角英数字混合であれば登録できる" do
+      @user.password = "abc123"
+      @user.password_confirmation = 'abc123'
+      expect(@user).to be_valid
+    end
+    it "パスワードとパスワード（確認）が一致していれば登録できる" do
+      @user.password = '123abc'
+      @user.password_confirmation = '123abc'
+      expect(@user).to be_valid
+    end
+    it "family_nameが空でなければ登録できる" do
+      @user.family_name = "名字"
+      expect(@user).to be_valid
+    end
+    it "first_nameが空でなければ登録できる" do
+      @user.first_name = "名前"
+      expect(@user).to be_valid
+    end
+    it "family_name_kanaが空でなければ登録できる" do
+      @user.family_name_kana = "ミョウジ"
+      expect(@user).to be_valid
+    end
+    it "first_name_kanaが空でなければ登録できる" do
+      @user.first_name_kana = "ナマエ"
+      expect(@user).to be_valid
+    end
+    it "family_name_kanaが全角だと登録できる" do
+      @user.family_name_kana = "ミョウジ"
+      expect(@user).to be_valid
+    end
+    it "first_name_kanaが全角だと登録できる" do
+      @user.first_name_kana = "ナマエ"
+      expect(@user).to be_valid
+    end
+    it "birth_dateが空でなければ登録できる" do
+      @user.birth_date = "2000-01-01"
+      expect(@user).to be_valid
+    end
+  end
+
+  end
+
