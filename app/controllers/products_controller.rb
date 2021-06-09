563e38fc02e46b68c9cd2b375406228ebdd81a03
kaleid-eminA <kaleidliner.illyasviel@gmail.com>
2021-06-09 12:51:20 +0900

    updateアクション及び直打ち禁止

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_products,only: [:edit]
+
+
   def index
     @products = Product.order('created_at DESC')
   end
@@ -24,6 +27,15 @@ class ProductsController < ApplicationController
   def edit
     @products = Product.find(params[:id])
   end
+  
+  def update
+    @products = Product.find(params[:id])
+    @products.update(products_params)
+    redirect_to show
+  end
+
+  
+
 
   private
 
@@ -31,4 +43,12 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name, :description, :condition_id, :shipping_cost_id, :prefecture_id, :days_to_ship_id,
                                     :category_id, :price, :image).merge(user_id: current_user.id)
   end
+
+  def correct_products
+    @products = Product.find(params[:id])
+unless @products.user.id == current_user.id
+  redirect_to root_path
+end
+end
+
 end
