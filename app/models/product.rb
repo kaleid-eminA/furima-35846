aa30274b8ca7c1c9c27648ff4c830c82c4bfa4a8
kaleid-eminA <kaleidliner.illyasviel@gmail.com>
2021-06-14 18:06:19 +0900

    最高金額　修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,6 +23,6 @@ class Product < ApplicationRecord
       validates :days_to_ship_id
     end
     validates :price, format: { with: VALID_PRICE_REGEX },
-                      numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 99_999_999 }
+                      numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 end
